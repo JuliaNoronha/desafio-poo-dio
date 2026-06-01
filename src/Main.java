85e7219aa4945fed18bdc793a47816339f59836b
Julia Noronha <105559554+JuliaNoronha@users.noreply.github.com>
2026-05-31 22:52:44 -0300

    criação de classes e seus objetos

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import br.com.dio.desafio.dominio.Curso;
+
+public class Main {
+    public static void main(String[] args){
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("description");
+        curso1.setCargaHoraria(8);
+
+        System.out.println(curso1);
+    }
+}
